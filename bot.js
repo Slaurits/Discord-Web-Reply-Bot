3ff9a264943f0bd82e7e454690a5b7b7d681b266
Laurits Volsgaard <laurits.volsgaard@gmail.com>
2022-09-06 21:39:52 +0200

    added the code and configs

---
+++ bot.js
@@ -0,0 +1,49 @@
+var Discord = require("discord.js");
+var client = new Discord.Client();
+var express = require("express");
+var fs = require("fs");
+var messages = [];
+var eventstreams = [];
+var app = express();
+client.on('ready', () => {
+    console.log("logged into discord");
+});
+client.on("message", msg => {
+    if (msg.content === "ping") {
+        msg.reply("pong");
+    }
+    messages.push(msg);
+    var data = {
+        content: msg.content,
+        msgindex: messages.indexOf(msg),
+        tag: msg.author.tag
+    };
+    for (i in eventstreams) {
+        eventstreams[i].write(`data: ${JSON.stringify(data)}\n\n`);
+        eventstreams[i].flushHeaders();
+    }
+});
+client.login("MTAxMjc0NjQ0NTE4MDM4MzI5Mw.GrsCGa.M5F8ukjOoy_2sWraYLWTkRILIy3Mzmy5aUH_oA");
+app.get("/", function(request, response) {
+    response.writeHead(200, {"content-type":"text/html"});
+    fs.createReadStream("index.html").pipe(response);
+});
+app.get('/sendmsg/:data', function (request,response) {
+    var currentdata = JSON.parse(request.params.data);
+    messages[currentdata.msgindex].reply(currentdata.reply);
+    response.end();
+});
+app.get("/messages",function(request,response) {
+    response.writeHead(200,{
+        "content-type":"text/event-stream",
+        "cache-control":"no-cache",
+        "access-control-allow-origin":"*",
+        "connection":"keep-alive"
+    });
+    response.write("data: connected!\n\n");
+    eventstreams.push(response);
+    response.flushHeaders();
+})
+
+app.listen(5500);
+console.log("app is running at http://localhost:5500");
